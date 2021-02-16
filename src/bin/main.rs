880a9a5e4e9c27da7b72c4606fc8413551cb6776
Aatif Syed <aatif.syed@metaswitch.com>
2021-02-16 15:14:21 +0000

    can remove a bunch of stuff, but it's buggy

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,8 @@
 use idocker::Listable;
-use shiplift::{self, Container, ContainerListOptions, Docker};
+use shiplift::{
+    self, Container, ContainerListOptions, Docker, Image, ImageListOptions, Network,
+    NetworkListOptions, Volume,
+};
 use std::error::Error;
 use structopt::StructOpt;
 use tokio;
@@ -30,7 +33,7 @@ enum VolumeOpt {
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(about = "Interactively select docker objects on which to perform operations")]
+#[structopt(about)]
 enum Opt {
     Container(ContainerOpt),
     Image(ImageOpt),
@@ -41,17 +44,17 @@ enum Opt {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let subcommand = Opt::from_args();
+    let docker = Docker::new();
 
     match subcommand {
-        Opt::Container(action) => subcommand_container(action).await,
-        Opt::Image(action) => subcommand_image(action).await,
-        Opt::Network(action) => subcommand_network(action).await,
-        Opt::Volume(action) => subcommand_volume(action).await,
+        Opt::Container(action) => subcommand_container(docker, action).await,
+        Opt::Image(action) => subcommand_image(docker, action).await,
+        Opt::Network(action) => subcommand_network(docker, action).await,
+        Opt::Volume(action) => subcommand_volume(docker, action).await,
     }
 }
 
-async fn subcommand_container(action: ContainerOpt) -> Result<(), Box<dyn Error>> {
-    let docker = Docker::new();
+async fn subcommand_container(docker: Docker, action: ContainerOpt) -> Result<(), Box<dyn Error>> {
     let opts = ContainerListOptions::builder().all().build();
     let containers = docker.containers();
     let selected = containers.interactively_select(&opts).await?;
@@ -61,53 +64,100 @@ async fn subcommand_container(action: ContainerOpt) -> Result<(), Box<dyn Error>
         .collect();
 
     match action {
-        ContainerOpt::Rm { force } => remove_containers(force, selected),
+        ContainerOpt::Rm { force } => {
+            if force {
+                for container in &selected {
+                    match container.kill(None).await {
+                        Err(err) => {
+                            eprintln!("Couldn't kill container {}: {}", container.id(), err)
+                        }
+                        _ => (),
+                    };
+                }
+            };
+            for container in &selected {
+                match container.delete().await {
+                    Err(err) => eprintln!("Couldn't delete container {}: {}", container.id(), err),
+                    _ => (),
+                }
+            }
+            Ok(())
+        }
         ContainerOpt::Inspect => unimplemented!(),
     }
-    .await?;
-    Ok(())
 }
 
-async fn remove_containers(
-    force: bool,
-    containers: Vec<Container<'_>>,
-) -> Result<(), Box<dyn Error>> {
-    if force {
-        for container in &containers {
-            match container.kill(None).await {
-                Err(err) => eprintln!("Couldn't kill container {}: {}", container.id(), err),
-                _ => (),
-            };
-        }
-    };
-    for container in &containers {
-        match container.delete().await {
-            Err(err) => eprintln!("Couldn't delete container {}: {}", container.id(), err),
-            _ => (),
-        }
-    }
-    Ok(())
-}
+async fn subcommand_image(docker: Docker, action: ImageOpt) -> Result<(), Box<dyn Error>> {
+    let opts = ImageListOptions::builder().all().build();
+    let images = docker.images();
+    let selected = images.interactively_select(&opts).await?;
+    let selected: Vec<_> = selected
+        .iter()
+        .map(|rep| Image::new(&docker, rep.id.to_owned()))
+        .collect();
 
-async fn subcommand_image(action: ImageOpt) -> Result<(), Box<dyn Error>> {
     match action {
-        ImageOpt::Rm => unimplemented!(),
+        ImageOpt::Rm => {
+            for image in &selected {
+                match image.delete().await {
+                    Err(err) => {
+                        eprintln!(
+                            "Couldn't delete image {}: {}",
+                            match image.inspect().await {
+                                Ok(details) => details.id,
+                                Err(_) => "<Couldn't get ID>".to_owned(),
+                            },
+                            err
+                        )
+                    }
+                    _ => (),
+                }
+            }
+        }
         ImageOpt::Inspect => unimplemented!(),
     };
     Ok(())
 }
 
-async fn subcommand_network(action: NetworkOpt) -> Result<(), Box<dyn Error>> {
+async fn subcommand_network(docker: Docker, action: NetworkOpt) -> Result<(), Box<dyn Error>> {
+    let opts = NetworkListOptions::default();
+    let networks = docker.networks();
+    let selected = networks.interactively_select(&opts).await?;
+    let selected: Vec<_> = selected
+        .iter()
+        .map(|rep| Network::new(&docker, rep.id.to_owned()))
+        .collect();
     match action {
-        NetworkOpt::Rm => unimplemented!(),
+        NetworkOpt::Rm => {
+            for network in &selected {
+                match network.delete().await {
+                    Err(err) => eprintln!("Couldn't delete network {}: {}", network.id(), err),
+                    _ => (),
+                }
+            }
+        }
         NetworkOpt::Inspect => unimplemented!(),
     };
     Ok(())
 }
 
-async fn subcommand_volume(action: VolumeOpt) -> Result<(), Box<dyn Error>> {
+async fn subcommand_volume(docker: Docker, action: VolumeOpt) -> Result<(), Box<dyn Error>> {
+    let opts = ();
+    let volumes = docker.volumes();
+    let selected = volumes.interactively_select(&opts).await?;
+    let selected: Vec<_> = selected
+        .iter()
+        .map(|rep| Volume::new(&docker, rep.name.to_owned()))
+        .collect();
     match action {
-        VolumeOpt::Rm => unimplemented!(),
+        VolumeOpt::Rm => {
+            for volume in &selected {
+                match volume.delete().await {
+                    Err(err) => eprintln!("Couldn't delete a volume: {}", err),
+                    _ => (),
+                }
+            }
+        }
         VolumeOpt::Inspect => unimplemented!(),
     };
     Ok(())
